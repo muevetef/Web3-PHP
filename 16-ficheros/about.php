a804e0e70af4f33493adc9ab82b0df9e4f4f98bc
Mi nombre <easyrobotics2@gmail-com>
2024-05-09 15:00:13 +0200

    fopen

---
+++ 16-ficheros/about.php
@@ -0,0 +1,1 @@
+<?php echo '<h1>Sobre mí</h1>'?>
---
+++ 16-ficheros/index.php
@@ -0,0 +1,20 @@
+<?php
+//Abrir archivo
+$archivo = fopen("archivo.txt", "a+");
+
+var_dump($archivo);
+//leer hasta el final
+while (!feof($archivo)) {
+    $contenido = fgets($archivo);
+    echo '<p>' . $contenido . '</p>';
+}
+
+
+
+//Escribir
+fwrite($archivo, date("d-m-Y h:i:s") . "-****Soy un texto añadido desde php****\n");
+fclose($archivo);
+
+$about = fopen("about.php", "a+");
+fwrite($about, "<?php echo '<h1>Sobre mí</h1>'?>");
+fclose($about);
